aaf9bac4f3cf292fb6194db201e5f2cc6f6f4d45
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-02 11:22:18 +0200

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { GameShopProvider } from "./context/GameContext";
-
+import DefaultLayout from "./layouts/DefaultLayout.jsx";
+import Homepage from "./pages/Homepage";
 export default function App() {
   return (
     <GameShopProvider>
       <BrowserRouter>
         <Routes>
-          {/* <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/videogames" element={<VideoGamesList />} /> 
-          <Route path="/videogames/:id" element={<DetailGame />} /> 
-          </Route> */}
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </GameShopProvider>
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,18 +1,15 @@
-import { Outlet } from react-router-dom;
+import { Outlet } from "react-router";
+import Header from "../pages/Header";
+import Footer from "../pages/Footer";
+export default function DefaultLayout() {
+  return (
+    <>
+      <Header />
 
-export default function DefaultLayout () {
-    return (
-        <>
-            <header>
-
-            </header>
-            
-            <main>
-                <Outlet />
-            </main>
-            <footer>
-
-            </footer>
-        </>
-    )
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }

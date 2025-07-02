844a669e6afeb648ac846eafeef60f6eb08c719c
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-02 14:03:28 +0200

    uxHomepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,6 @@
 import PlatformsLinkComponent from "../components/PlatformsLinkComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlaystation } from "@fortawesome/free-brands-svg-icons";
 
 export default function Homepage() {
   return (
@@ -6,11 +8,17 @@ export default function Homepage() {
       <div className="wrapper-homepage vh-100">
         <div className="container">
           <div className="jumbotron">
-            <img src="public\img\retro_gaming_room_8_bit_art-t2.jpg" alt="" />
+            <h1 className="logobool">BOOLSTOP</h1>
           </div>
-          <div className="logocontainer d-flex justify-content-center flex-row">
+
+          <div className="logocontainer d-flex justify-content-center mt-5 ">
             <PlatformsLinkComponent linkPage={"/videogames/ps5"}>
-              ps5
+              <FontAwesomeIcon
+                className="iconplat"
+                icon={faPlaystation}
+                size="2xl"
+                style={{ color: "#ffffff" }}
+              />
             </PlatformsLinkComponent>
           </div>
         </div>

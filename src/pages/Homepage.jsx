7413ad20b254a6cdea3336199ff37938a421b7a7
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-02 14:21:47 +0200

    fixMargediMERDA

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,16 +4,20 @@ import { dataPath } from "../data/dataPath";
 export default function Homepage() {
   return (
     <>
-      <div className="container">
-        <div className="jumbotron"></div>
-        <div className="logocontainer d-flex justify-content-center flex-row"></div>
-        <section className="link-platforms">
-          {dataPath.map((data, i) => (
-            <PlatformsLinkComponent key={i} linkPage={data.path}>
-              <i className={data.icon}></i>
-            </PlatformsLinkComponent>
-          ))}
-        </section>
+      <div className="wrapper-homepage vh-100">
+        <div className="container">
+          <div className="jumbotron">
+            <h1 className="logobool">BOOLSTOP</h1>
+          </div>
+          <div className="logocontainer d-flex justify-content-center flex-row"></div>
+          <section className="link-platforms">
+            {dataPath.map((data, i) => (
+              <PlatformsLinkComponent key={i} linkPage={data.path}>
+                <i className={data.icon}></i>
+              </PlatformsLinkComponent>
+            ))}
+          </section>
+        </div>
       </div>
     </>
   );

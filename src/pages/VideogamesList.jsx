e48524c274989e38e7210b5af2439409c9e9c718
dannyarena <arenadanny71@gmail.com>
2025-07-02 11:30:40 +0200

    create videogames list structrure

---
+++ src/pages/VideogamesList.jsx
@@ -0,0 +1,10 @@
+const VideogamesList = () => {
+    return (
+        <div>
+            <h1>Tutti i giochi</h1>
+            {/* { lista dei giochi...} */}
+        </div>
+    );
+};
+
+export default VideogamesList;

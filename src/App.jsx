3123fb3a3d634e5975aacf1fd6eb0d107505da05
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-02 10:00:02 +0200

    indexEcomponentsFolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Main from "./components/Main";
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-   <div></div>
-  )
+    <>
+      {/* <Header />
+      <Main />
+      <Footer /> */}
+    </>
+  );
 }
-
-export default App

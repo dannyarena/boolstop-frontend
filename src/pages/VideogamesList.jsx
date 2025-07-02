b59b66e019fef63816269acebd240e8c3a1163ed
dannyarena <arenadanny71@gmail.com>
2025-07-02 12:22:56 +0200

    fix

--- src/pages/VideogamesList.jsx
+++ src/pages/VideogamesList.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const [videogames, setVideogames] = useState([]);
 const VideogamesList = () => {
+const [videogames, setVideogames] = useState([]);
+useEffect(() => {
+    fetch("http://localhost:3000/videogames")
+    .then((response) => response.json())
+    .then((data) => {
+        setVideogames(data.results); // salva i dati
+    })
+    .catch((error) =>{
+        console.error("Errore durante la ricezione dei dati", error);
+    });
+}, []);
+
     return (
         <div>
             <h1>Tutti i giochi</h1>

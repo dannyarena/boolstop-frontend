38d67dc1a765a53ea2ac4e43f92be7c343909611
fabioadelfio <adelfiofabio07@gmail.com>
2025-07-02 11:20:49 +0200

    fix DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { GameShopProvider } from "./context/GameContext";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return (
     <GameShopProvider>
       <BrowserRouter>
         <Routes>
-          {/* <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/videogames" element={<VideoGamesList />} /> 
-          <Route path="/videogames/:id" element={<DetailGame />} /> 
-          </Route> */}
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/videogames" element={<VideoGamesList />} />
+            <Route path="/videogames/:id" element={<DetailGame />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </GameShopProvider>

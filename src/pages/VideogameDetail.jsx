5d7ab91d7918cef3a8537f8b77b39ec112effbcc
dannyarena <arenadanny71@gmail.com>
2025-07-03 21:32:30 +0200

    add funtcion to add videogame into cart

--- src/pages/VideogameDetail.jsx
+++ src/pages/VideogameDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 
 const VideogamesDetail = () => {
     // con slug leggiamo i parametri dinamici della rotta
@@ -18,9 +18,40 @@ const VideogamesDetail = () => {
         });
     }, [slug]);
 
+    if(!videogame) return <p>Caricamento...</p>;
+
+    const addToCart = () => {
+        const ItemToAdd = {
+            id: videogame.id,
+            name: videogame.name,
+            price: videogame.original_price,
+            image: videogame.image,
+            quantity: 1
+        };
+
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        const existingItemIndex = cart.findIndex(item => item.id === ItemToAdd.id);
+
+        if (existingItemIndex !== -1) {
+            cart[existingItemIndex].quantity +=1;
+        } else {
+            cart.push(ItemToAdd);
+        }
+    }
+
     return (
-        <div>
-            <h1>Dettaglio videogioco</h1>
+        <div className="container py-5">
+            <h1>{videogame.name}</h1>
+            <img
+                src={`/img/${videogame.image}`}
+                alt={videogame.name}
+                className="img-fluid my-3"
+                style={{ maxWidth: "400px" }}
+                />
+                <p>{videogame.description}</p>
+                <p><strong>Prezzo:</strong>{videogame.original_price}</p>
+                <p><strong>Piattaforma:</strong>{videogame.platform}</p>
+                <p><strong>PEGI:</strong>{videogame.pegi}</p>
         </div>
     );
 };

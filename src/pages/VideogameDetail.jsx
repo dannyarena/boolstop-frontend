499c87f357fea6176062d8f8a0f35f241817637b
dannyarena <arenadanny71@gmail.com>
2025-07-03 19:57:16 +0200

    Add state management for VideogameDetail component

--- src/pages/VideogameDetail.jsx
+++ src/pages/VideogameDetail.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 const VideogamesDetail = () => {
+    // con slug leggiamo i parametri dinamici della rotta
+    const { slug } = useParams();
+    const [videogame, setVideogames] = useState(null);
+    console.log("Slug ricevuto dall'URL;",slug);
+
     return (
         <div>
             <h1>Dettaglio videogioco</h1>

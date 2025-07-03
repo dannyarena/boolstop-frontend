3b0821cdf2ce46d7f680ea2d1b9b18379ad841c8
dannyarena <arenadanny71@gmail.com>
2025-07-03 20:05:43 +0200

    add fetch to videogamedetail component

--- src/pages/VideogameDetail.jsx
+++ src/pages/VideogameDetail.jsx
@@ -4,8 +4,19 @@ import { useParams } from "react-router";
 const VideogamesDetail = () => {
     // con slug leggiamo i parametri dinamici della rotta
     const { slug } = useParams();
-    const [videogame, setVideogames] = useState(null);
+    const [videogame, setVideogame] = useState(null);
     console.log("Slug ricevuto dall'URL;",slug);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/videogames/slug/${slug}`)
+        .then(res => res.json())
+        .then(data => {
+            setVideogame(data);
+        })
+        .catch(err => {
+            console.error("Errore nella ricezione dei dati", err);
+        });
+    }, [slug]);
 
     return (
         <div>

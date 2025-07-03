017b36eeca0e38d247cce8eb216be81e946e5900
dannyarena <arenadanny71@gmail.com>
2025-07-03 19:35:37 +0200

    create videogamedetail component

---
+++ src/pages/VideogameDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideogamesDetail = () => {
+    return (
+        <div>
+            <h1>Dettaglio videogioco</h1>
+        </div>
+    );
+};
+
+export default VideogamesDetail

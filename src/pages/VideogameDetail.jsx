afadbf28ade052b68b30fc66f673ba52b8cb79f7
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-04 18:17:23 +0200

    fixPrice

--- src/pages/VideogameDetail.jsx
+++ src/pages/VideogameDetail.jsx
@@ -55,55 +55,11 @@ const VideogamesDetail = () => {
       setAmountInCart(item ? item.amount : 0);
     }
   }, [videogame]);
+  if (!videogame) return <p>Caricamento...</p>;
 
   const dsc = (videogame.original_price * videogame.discount_percentage) / 100;
   const price = (videogame.original_price - dsc).toFixed(2);
 
-  return (
-    <div className="container bg-light bg-gradient text-center py-5 rounded-5 shadow-lg">
-      <h1>{videogame.name}</h1>
-      <img
-        src={`${videogame.image}`}
-        alt={videogame.name}
-        className="img-fluid my-3"
-        style={{ maxWidth: "400px" }}
-      />
-      <h4>{videogame.description}</h4>
-      <p>
-        <strong>Genere: </strong>
-        {videogame.genres}
-      </p>
-      <p className="fs-10 text-decoration-line-through">
-        {videogame.original_price} &euro;
-      </p>
-      <p>
-        <strong className="fs-5">
-          {price}
-          &euro;
-        </strong>
-      </p>
-      <p>
-        <strong>Piattaforma: </strong>
-        {videogame.platform}
-      </p>
-      <p>
-        <strong>Data di rilascio: </strong>
-        {new Date(videogame.release_date).toLocaleDateString("it-IT")}
-      </p>
-      <p>
-        <strong>PEGI: </strong>
-        {videogame.pegi}
-      </p>
-      <p>
-        {Array.from({ length: 5 }).map((_, i) =>
-          i < Math.round(videogame.rating) ? (
-            <i key={i} className="bi bi-star-fill text-warning fs-3"></i>
-          ) : (
-            <i key={i} className="bi bi-star text-warning fs-3"></i>
-          )
-        )}
-      </p>
-  if (!videogame) return <p>Caricamento...</p>;
   return (
     <>
       <div className="container bg-light bg-gradient text-center py-5 rounded-5 shadow-lg">
@@ -119,10 +75,21 @@ const VideogamesDetail = () => {
           <strong>Genere: </strong>
           {videogame.genres}
         </p>
-        <p>
-          <strong>Prezzo: &euro; </strong>
-          {videogame.original_price}
-        </p>
+        {videogame.discount_percentage ? (
+          <>
+            <p className="fs-10 text-decoration-line-through">
+              {videogame.original_price} &euro;
+            </p>
+            <p>
+              <strong className="fs-5">
+                {price}
+                &euro;
+              </strong>
+            </p>
+          </>
+        ) : (
+          <p>{videogame.original_price} &euro;</p>
+        )}
         <p>
           <strong>Piattaforma: </strong>
           {videogame.platform}

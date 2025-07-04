ccd9c3b257af2a1edbabdf9b2226ffeb38421de1
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-04 10:31:57 +0200

    fiximg

--- src/components/CardGameDamb.jsx
+++ src/components/CardGameDamb.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function CardGameDamb({ game, platform }) {
   return (
     <div className="cardGame col-sm-6 col-md-4" key={game.id}>
-      <img className="img w-100" src={`../cover-game/${game.image}`} alt={game.name} />
+      <img className="img w-100" src={game.image} alt={game.name} />
       <div className="infoCard text-center p-3">
         <h5 className="card-title">{game.name}</h5>
         <p className="card-text">{game.description}</p>

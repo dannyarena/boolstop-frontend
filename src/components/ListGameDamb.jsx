4a9125f8d4bd8b31396425a010c394cfbf234c7c
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-08 17:05:05 +0200

    cssListDmb

--- src/components/ListGameDamb.jsx
+++ src/components/ListGameDamb.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router";
 
 export default function ListGameDamb({ game }) {
   return (
-    <li className="d-flex justify-content-between align-items-center mb-1" key={game.id}>
-      {game.name}{" "}
-      <Link to={`/videogame/${game.slug}`}>
-        <i className="bi bi-arrow-right-circle text-warning"></i>
-      </Link>
-    </li>
+    <Link to={`/videogame/${game.slug}`} className="no-link-style">
+      <li
+        className="d-flex justify-content-between align-items-center mb-1 listGameItem"
+        key={game.id}
+      >
+        {game.name} <i className="bi bi-arrow-right-circle text-dark fs-1"></i>
+      </li>
+    </Link>
   );
 }

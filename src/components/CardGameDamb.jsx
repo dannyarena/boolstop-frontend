bbc9d0f3970fc77dac6a39f081c9c4869b2d8098
fabioadelfio <adelfiofabio07@gmail.com>
2025-07-08 11:20:07 +0200

    style cards

--- src/components/CardGameDamb.jsx
+++ src/components/CardGameDamb.jsx
@@ -65,46 +65,54 @@ export default function CardGameDamb({ game, platform, isInWishlist, onToggleWis
   };
 
   return (
-    <div className="cardGame col-sm-6 col-md-4" key={game.id}>
-      <img className="img w-100" src={game.image} alt={game.name} />
-      <div className="infoCard text-center p-0">
-        <h5 className="card-title">{game.name}</h5>
-        <p className="card-text">{game.description}</p>
-        <p className="card-text">{game.genre}</p>
-        <p className="card-text">
-          <small className="text">Piattaforma: {game.platform}</small>
-        </p>
-        <Link
-          to={platform ? `/videogame/${game.slug}` : `/videogame/${game.slug}`}
-          className="btn btn-warning"
-        >
-          Scopri
-        </Link>
+    <div className="col-sm-6 col-md-4">
+      <div className="cardGame" key={game.id}>
+        <img className="img w-100" src={game.image} alt={game.name} />
+        <div className="infoCard">
+          <h5>{game.name}</h5>
+          <p>{game.description}</p>
+          <p>
+            <strong>Genere:</strong> {game.genres.join(", ")}
+          </p>
+          <p>
+            <small>Piattaforma: {game.platform}</small>
+          </p>
+          <Link
+            to={platform ? `/videogame/${game.slug}` : `/videogame/${game.slug}`}
+            className="btn btn-warning"
+          >
+            Scopri
+          </Link>
 
-        <div className="d-flex justify-content-center align-items-center mt-3">
-          {amountInCart === 0 ? (
-            <button className="btn btn-success p-3 mx-1" onClick={() => addToCart()}>
-              <i className="bi bi-cart-plus"></i>
-            </button>
-          ) : (
-            <div className="mt-2">
-              <button className="btn btn-success fs-4 px-3 mx-1" onClick={() => addToCart()}>
-                +
+          <div className="d-flex justify-content-center align-items-center mt-3 gap-3">
+            {amountInCart === 0 ? (
+              <button className="btn btn-success p-3 mx-1" onClick={() => addToCart()}>
+                <i className="bi bi-cart-plus"></i>
               </button>
-              <button className="btn btn-success fs-4 px-3 mx-1" onClick={() => removeFromCart()}>
-                -
-              </button>
-              <div>(quantità: {amountInCart})</div>
+            ) : (
+              <div className="mt-2">
+                <button className="btn btn-success fs-4 px-3 mx-1" onClick={() => addToCart()}>
+                  +
+                </button>
+                <button className="btn btn-success fs-4 px-3 mx-1" onClick={() => removeFromCart()}>
+                  -
+                </button>
+                <div>(quantità: {amountInCart})</div>
+              </div>
+            )}
+
+            <div className="heart" onClick={() => onToggleWishlist(game.id)}>
+              {isInWishlist ? (
+                <i className="bi bi-heart-fill"></i>
+              ) : (
+                <i className="bi bi-heart"></i>
+              )}
             </div>
-          )}
-
-          <div className="heart" onClick={() => onToggleWishlist(game.id)}>
-            {isInWishlist ? <i className="bi bi-heart-fill"></i> : <i className="bi bi-heart"></i>}
           </div>
+          <button className="btn btn-primary mt-4" onClick={buyNow}>
+            Compra ora
+          </button>
         </div>
-        <button className="btn btn-primary mt-4" onClick={buyNow}>
-          Compra ora
-        </button>
       </div>
     </div>
   );
